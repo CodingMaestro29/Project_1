0859d6b408b900ee8069b4ed10b29d80110e46d4
unknown <amit@uiux.studio>
2024-03-11 21:11:09 +0530

    updated code

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -39,6 +39,23 @@ class Student extends Authenticatable implements AuthenticatableContract
         'find',
     ];
 
+     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 
 
 }
